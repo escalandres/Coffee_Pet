8936f95a180bc0c744da16901de3a4f4e0862e97
escalandres <aescalaa1900@alumno.ipn.mx>
2021-09-25 22:32:37 -0500

    New changes, reorganized folder

---
+++ app.js
@@ -0,0 +1,18 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+
+    Post.find({}, function(err, posts){
+      res.render("index");
+    });
+  });
