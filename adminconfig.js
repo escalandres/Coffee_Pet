07b540544dc60de92e9c91b9bd3f0ce37ce06c61
escalandres <aescalaa1900@alumno.ipn.mx>
2021-10-14 14:29:50 -0500

    New changes in backend, more ejs, error page added

---
+++ adminconfig.js
@@ -0,0 +1,6 @@
+const admin = {
+    username: 'admin',
+    password: 'admin',
+    key: 'admin'
+}
+module.exports = admin;
--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const config = require("./dbconfig");
-const rest = new(require('rest-mssql-nodejs'))(config);
+// const rest = new(require('rest-mssql-nodejs'))(config);
 const sql = require('mssql')
-const port = 3000;
+const port = 3001;
+const admin = require("./adminconfig");
+let logAdmin = 0;
 // setTimeout(async()=>{
 //   const resultado = await rest.executeQuery('select * from customers');
   
@@ -31,13 +33,22 @@ var  executeQuery = function(res, query){
 					res.send(err);
 				}
 				else {
-          console.log(recordset);
+          			console.log(recordset);
 					res.send(recordset);
+					connection.close();
 				}
 			});
 		}
 	});
 }
+
+//  var query1 = "select * from customers";
+//  var query2 = "insert into customers(id,nombre,apellido,edad) values (5,'John','Smith',23)";
+//  var query3 = "insert into products(id,nombre,precio) values (1000,'Guante',23)";
+//  var query4 = "delete from customers where id=5";
+// 	var query5 = "update customers set edad = 32 where id=5";
+// executeQuery (res , query);
+//   executeQuery (res , query4);
 const app = express();
 app.set('view engine', 'ejs');
 
@@ -45,13 +56,68 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-  var query1 = "select * from customers";
-  // var query = "insert into customers(id,nombre,apellido,edad) values (5,'John','Smith',23)";
-  // executeQuery (res , query);
-  executeQuery (res , query1);
+	res.render("pages/index");
 });
-  
-  app.listen(port, () => {
+
+app.get("/login",function(req,res){
+	res.render("pages/login");
+})
+
+app.get("/registro",function(req,res){
+	res.render("pages/registro");
+})
+
+app.get("/about",function(req,res){
+	res.render("pages/about");
+})
+
+app.get("/pets",function(req,res){
+	res.render("pages/pets");
+})
+
+app.get("/login-admin",function(req,res){
+	res.render("pages/login-admin");
+})
+
+app.get("/logout",function(req,res){
+	logAdmin = 0;
+	console.log("Sesion admin cerrada");
+	res.redirect("/");
+})
+
+app.get("/admin",function(req,res){
+	if(logAdmin === 1){
+		res.render("pages/admin");
+	}
+	else{
+		res.redirect("error");
+	}
+	
+})
+
+
+app.get("/error",function(req,res){
+	res.render("pages/error");
+})
+
+app.post("/login-admin", function(req, res){
+
+	const user = {
+		username: req.body.username,
+		password: req.body.password,
+		key: req.body.key
+	};
+	if(user.username === admin.username && user.password === admin.password && user.key === admin.key){
+		console.log("Si se pudo");
+		logAdmin = 1;
+		res.redirect("admin");
+	}
+	else{
+		res.redirect("error");
+	}
+});
+
+app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
-  });
-  
+	
+});
