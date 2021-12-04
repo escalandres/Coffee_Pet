7ee99fd83acfaef0ce68e5d14d101ec0c82b458f
escalandres <aescalaa1900@alumno.ipn.mx>
2021-12-04 00:47:24 -0600

    new2

--- dbconfig.js
+++ dbconfig.js
@@ -1,7 +1,7 @@
 const config={
     user:"sa",
     password:"di mi nombre",
-    server:"SKLA-L",//169.254.210.155 169.254.138.141 SKLA-L
+    server:"192.168.56.1",//169.254.210.155 169.254.138.141 SKLA-L
     database:"prueba",
     port:1433,
     options:{
@@ -9,7 +9,7 @@ const config={
         enableArithAbort: true,
         enablearithabort: true,
         encrypt:false,
-        instancename: '', //MSSQLSERVER
+        instancename: 'MSSQLSERVER', //MSSQLSERVER
         connectionTimeout: 999999999,
         requestTimeout: 999999999
     }
